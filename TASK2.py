04683bc9033c511be1c5685a561659f52d3c2512
SATHISHKUMAR-01 <74890906+SATHISHKUMAR-01@users.noreply.github.com>
2020-11-24 14:12:22 +0530

    Add files via upload

---
+++ TASK2.py
@@ -0,0 +1,19 @@
+
+#EXERCISE 1
+a=b=c=100
+print(a/10)
+print(b*50)
+print(c+60)
+
+#EXERCISE 2
+word="begin"
+print(word.replace(word[2],"G"))
+
+#EXERCISE 3
+a=100
+b=50.5
+a=float(a)
+print(type(a))
+b=int(b)
+print(type(b))
+
