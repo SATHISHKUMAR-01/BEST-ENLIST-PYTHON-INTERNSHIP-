4921825bedcac6c4aaf9a489835600f80ed09c37
SATHISHKUMAR-01 <74890906+SATHISHKUMAR-01@users.noreply.github.com>
2020-11-29 15:51:42 +0530

    Add files via upload

---
+++ task7 finding sys path.py
@@ -0,0 +1,6 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import sys
+>>> sys.path
+['', 'C:\\Users\\sathish\\AppData\\Local\\Programs\\Python\\Python39\\Lib\\idlelib', 'C:\\Users\\sathish\\AppData\\Local\\Programs\\Python\\Python39\\python39.zip', 'C:\\Users\\sathish\\AppData\\Local\\Programs\\Python\\Python39\\DLLs', 'C:\\Users\\sathish\\AppData\\Local\\Programs\\Python\\Python39\\lib', 'C:\\Users\\sathish\\AppData\\Local\\Programs\\Python\\Python39', 'C:\\Users\\sathish\\AppData\\Local\\Programs\\Python\\Python39\\lib\\site-packages']
+>>> 
